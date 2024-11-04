05db91bc511698070a0c73469b1997872a4139f3
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-11-04 10:31:51 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,48 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="App"></div>
-      <h1>Rehyph</h1>
-      <p>Rehyph is a web app that helps you rephrase your text.</p>
+      <div className="hero">
+        <h1 className="logo">bhaka</h1>
+        <ul>
+          <li>
+            <a
+              href="https://cosmic-kheer-381ba4.netlify.app/"
+              target="_blank"
+            ></a>
+          </li>
+          <li>
+            <a href="html/oneshots.html">One-Shots</a>
+          </li>
+          <li>
+            <a href="./html/music.html">Music</a>
+          </li>
+          <li>
+            <a href="./html/merch.html">Merch</a>
+          </li>
+          <li>
+            <a href="https://www.patreon.com/" target="_blank">
+              Patreon
+            </a>
+          </li>
+        </ul>
+        <div className="social-media">
+          <a
+            href="https://twitter.com/breezytresbeau"
+            target="_blank"
+            rel="noopener"
+          >
+            <i className="ph ph-x-logo"></i>
+          </a>
+          <div className="vl"></div>
+          <a
+            href="https://www.instagram.com/breezytresbeau/"
+            target="_blank"
+            rel="noopener"
+          >
+            <i className="ph ph-instagram-logo"></i>
+          </a>
+        </div>
+      </div>
     </>
   );
 }
