e6da395a841b454fc27da76b087502c102838c27
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-11-03 22:34:15 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App"></div>
+      <h1>Rehyph</h1>
+      <p>Rehyph is a web app that helps you rephrase your text.</p>
+    </>
+  );
+}
+
+export default App;
