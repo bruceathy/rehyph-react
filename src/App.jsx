0a1a409f45241957a6e026c7359c6f99ab19bde1
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-11-07 18:18:06 -0500

    homepage update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <>
+      <Navbar />
       <div className="hero">
         <h1 className="logo">bhaka</h1>
         <ul>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1 className="logo-mini">
+        <a href="../index.html">BHAKA</a>
+      </h1>
+      <ul>
+        <li>
+          <a href="https://cosmic-kheer-381ba4.netlify.app/" target="_blank">
+            NOMAD
+          </a>
+        </li>
+        <li>
+          <a href="./oneshots.html">One-Shots</a>
+        </li>
+        <li>
+          <a href="./merch.html">Merch</a>
+        </li>
+        <li>
+          <a href="https://www.patreon.com/" target="_blank">
+            Patreon
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
